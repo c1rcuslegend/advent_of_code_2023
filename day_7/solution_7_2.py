c8a060ced3873efa816c7979bb04472edd9a5e2b
Illia Volkov <akelloillya@gmail.com>
2023-12-14 20:38:58 +0100

    Day 7

--- day_7/solution_7_2.py
+++ day_7/solution_7_2.py
@@ -3,12 +3,26 @@ import collections
 
 def Solution():
     PATH = "input.txt"
-    order = "AKQJT98765432"
+    order = "AKQT98765432J"
     with open(PATH) as file:
         table, bids = {x: [] for x in [5,7,9,11,13,17,25]}, {}
         for c, b in [_.strip().split() for _ in file.readlines()]:
             bids[c] = int(b)
-            table[sum([x ** 2 for x in collections.Counter(c).values()])].append(c)
+            count=collections.Counter(c)
+            temp=0
+            if "J" in count:
+                temp=count["J"]
+                del count["J"]
+            if not count:
+                table[temp**2].append(c)
+                continue
+            summ,maxx=0,max(count.values())
+            for v in count.values():
+                if v==maxx:
+                    v+=temp
+                    maxx=-1
+                summ+=v**2
+            table[summ].append(c)
         ans,count=0,1
         for arr in table.values():
             for s in sorted(arr, key=lambda word: [order.index(char) for char in word],reverse=True):
@@ -18,3 +32,4 @@ def Solution():
 
 
 print(Solution())
+
