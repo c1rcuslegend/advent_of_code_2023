59f24c345e3a92696e54028e2cadf6b542f92422
Illia Volkov <akelloillya@gmail.com>
2023-12-15 14:08:58 +0100

    Day 9

---
+++ day_9/solution_9_1.py
@@ -0,0 +1,18 @@
+def Solution():
+    PATH = "input.txt"
+    ans=0
+    with open(PATH) as file:
+        for line in file.readlines():
+            arr=[int(_) for _ in line.strip().split()]
+            stk=[arr[-1]]
+            while arr!=[0]*len(arr):
+                new_arr=[]
+                for i in range(1,len(arr)):
+                    new_arr.append(arr[i]-arr[i-1])
+                stk.append(new_arr[-1])
+                arr=new_arr[:]
+            ans+=sum(stk)
+    return ans
+
+print(Solution())
+
